6f20208bc3db104b9cc7514f2eaba1ee5bc8c21e
Gabriel Cabrera <gabycabper237@gmail.com>
2021-04-14 19:01:23 +0200

    add Script and collision

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    public static int coinsCount = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Coin.coinsCount++;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if(collider.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+      
+    }
+
+    private void OnDestroy()
+    {
+        Coin.coinsCount--;
+
+        if(Coin.coinsCount<=0)
+        {
+            Debug.Log("El Juego ha terminado, has ganado!");
+        }
+    }
+}
